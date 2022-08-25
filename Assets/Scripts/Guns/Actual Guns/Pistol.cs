d80c44101e6e5b9b6cc108180765285b16d8b9f0
Joel <fallendemongod@gmail.com>
2022-08-25 17:14:02 -0400

    Make Pistol class

---
+++ Assets/Scripts/Guns/Actual Guns/Pistol.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using Guns;
+using UnityEngine;
+
+public class Pistol : BaseGun
+{
+    //The pistol is a basic gun that has a single bullet per shot.
+    
+}
