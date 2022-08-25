575e73c03302d6ae0eba0f07b414fb2d60813eed
Joel <fallendemongod@gmail.com>
2022-08-25 17:14:20 -0400

    Update PlayerMovement.cs

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -16,6 +16,6 @@ public class PlayerMovement : MonoBehaviour
         float y = Input.GetAxisRaw("Vertical");
         Vector2 direction = new Vector2(x, y).normalized;
         
-        transform.Translate(direction * speed * Time.deltaTime);    
+        transform.Translate(direction * (speed * Time.deltaTime));    
     }
 }
