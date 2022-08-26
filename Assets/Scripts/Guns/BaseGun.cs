1c8373a0bc75a378b440a6f2d53f3b3c6e8f27dc
Joel <fallendemongod@gmail.com>
2022-08-26 16:12:50 -0400

    Organized BaseGun

--- Assets/Scripts/Guns/BaseGun.cs
+++ Assets/Scripts/Guns/BaseGun.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Guns
@@ -7,47 +8,57 @@ namespace Guns
 {
     public abstract class BaseGun : MonoBehaviour
     {
+        [Title("Fire Rate")] [SerializeField] private protected float fireRate;
 
+        [Title("Magazine Size")] [SerializeField]
+        private protected int magazineSize;
+
+        [SerializeField] private protected int currentMagazine;
         
-       [SerializeField] private protected float fireRate;
-       [SerializeField] private protected int magazineSize;
-       [SerializeField] private protected int currentMagazine;
-       [SerializeField] private protected float reloadTime;
-       [SerializeField] private protected float accuracy;
+        [Title("Reload Time")]
+        [SerializeField] private protected float reloadTime;
+        
+        [Title("Accuracy/Heat")]
+        [SerializeField] private protected float accuracy;
 
-       [SerializeField] private BaseBullet bullet;
-       
-       
+        [Title("Bullet Prefab")] [SerializeField]
+        private Transform bulletSpawn;
+        [SerializeField] private protected GameObject bulletPrefab;
+        [SerializeField,ReadOnly] private BaseBullet bullet;
+
+
+        [Title("Bullet Spawn Amount")]
         //Amount of bullets fired every shot
-        [SerializeField]private protected int bulletAmount;
+        [SerializeField] private protected int bulletAmount;
 
         private protected Action onFire;
         private protected Action onReload;
         private protected Action onEmpty;
         private protected Action onSwitch;
 
-        
+
         //public variables
         public bool autoFire;
-        
-        
+
+
         //Private variables
         private bool _isReloading;
         private float _fireRateTimer;
         private float _reloadTimer;
         private bool _canFire;
-        
+
         //Cache a waitforseconds for performance
         private WaitForSeconds _reloadWait;
 
         private void Start()
         {
             _reloadWait = new WaitForSeconds(reloadTime);
+            bullet = bulletPrefab.GetComponent<BaseBullet>();
         }
 
         public virtual void Fire()
         {
-            if(!_canFire) return;
+            if (!_canFire) return;
 
 
             if (currentMagazine > 0)
@@ -56,6 +67,8 @@ namespace Guns
                 currentMagazine--;
                 Debug.Log("Firing");
                 onFire?.Invoke();
+                
+                Instantiate(bulletPrefab,bulletSpawn.transform.position);
                 _canFire = false;
             }
             else
@@ -68,9 +81,9 @@ namespace Guns
         {
             //async reload
             if (_isReloading || currentMagazine == magazineSize) return;
-            
+
             _isReloading = true;
-            
+
             //TODO switch to normal timer for UI purposes
             StartCoroutine(StartReload());
         }
@@ -80,8 +93,8 @@ namespace Guns
             if (_isReloading) return;
 
             if (_canFire) return;
-            
-            if(_fireRateTimer > 0)
+
+            if (_fireRateTimer > 0)
             {
                 _fireRateTimer -= Time.deltaTime;
             }
@@ -96,8 +109,8 @@ namespace Guns
         {
             //Event that is called when reloading
             onReload?.Invoke();
-            
-            
+
+
             yield return _reloadWait;
             _isReloading = false;
             var bulletAmountToAdd = magazineSize - currentMagazine;
