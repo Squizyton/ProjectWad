cbd3bba812799f1ba647903bff94cd32da80ad9a
Joel <fallendemongod@gmail.com>
2022-08-25 21:24:58 -0400

    Create ability type for sorting purposes

---
+++ Assets/Scripts/Guns/Abilities/AbilityType.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityType : MonoBehaviour
+{
+    public enum Type
+    {
+    OnHit,
+    OnDeath,
+    OnFire,
+    OnReload,
+    OnMove,
+    }
+}
